b371289a270cfef0a4817a520a3d32c5c23d98fb
ervitis <vitomarti@gmail.com>
2017-06-28 21:37:20 +0200

    test: testing with circle and CD

---
+++ tests/DumbTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kufrases\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DumbTest extends TestCase {
+    public function testDumb() {
+        $this->assertTrue(true);
+    }
+}
